2ca97de5e4c8094c4731746ecdd3b19ab481e21c
Tanmai Shyam Thakre <tanmaithakre@gmail.com>
2026-07-22 22:56:14 +0530

    Time: 0 ms (100%), Space: 47.8 MB (96.45%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(right >= left){
+            int mid = (left+right)/2;
+            if (target == nums[mid]){
+                return mid;
+            }
+            if (target < nums[mid]){
+                right = mid-1;
+            }
+            if (target > nums[mid]){
+                left = mid+1;
+            }
+        }
+        return -1;
+    }
+}
